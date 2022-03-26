9dff4d303cc257490801bd4677c0037cf00f3c38
mo7medhasan <mo7med.hasan5@gmail.com>
2022-03-26 20:17:43 +0200

    create new folder and  file like (  post -feed -rightbar - share - Sidebar - Home and update in index to font customer   ) in cilent side

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
         <Topbar/>
         <div className="homeContainer">
-          <Sidebar/>
+          <Sidebar />
           <Feed/>
           <Rightbar/>
           </div>    
